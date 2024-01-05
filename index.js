70dff228f611d5cbc59339d3355fb9cfb28abd27
Lawani Jeremiah <lawanijeremiah23@gmail.com>
2024-01-05 04:44:30 +0100

    working progress

--- index.js
+++
@@ -1,8 +0,0 @@
-function changeImage(){
-    var image = document.getElementById("myImage");
-    if (image.scr.match("bulbon")){
-        image.src = "Images/bulb.jpg";
-    } else{
-        image.src = "Images/bulb.jpg";
-    }
-}
